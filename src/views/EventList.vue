34228d1c427d422480fb8fc44b1e901e83a740cc
Carlton Joseph <carlton.joseph@gmail.com>
2021-03-02 11:30:06 -0800

    add pagination

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -2,30 +2,60 @@
   <h1>Events For Good</h1>
   <div class="events">
     <EventCard v-for="event in events" :key="event.id" :event="event" />
+    <div class="pagination">
+      <router-link
+        v-if="page - 1 > 0"
+        :to="{ name: 'EventList', query: { page: page - 1 } }"
+        rel="prev"
+      >
+        &lt;&lt; Page {{ page - 1 }}
+      </router-link>
+      <router-link
+        v-if="hasNextPage"
+        :to="{ name: 'EventList', query: { page: page + 1 } }"
+        rel="next"
+      >
+        Page {{ page + 1 }} &gt;&gt;
+      </router-link>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
+import { watchEffect } from 'vue'
 import EventCard from '@/components/EventCard.vue'
 import EventService from '../services/EventService'
 
 export default {
   name: 'EventList',
+  props: {
+    page: Number,
+    perPage: Number
+  },
   components: {
     EventCard
   },
   data() {
     return {
-      events: null
+      events: null,
+      totalEvents: 0
     }
   },
   async created() {
-    try {
-      const response = await EventService.getEvents()
-      this.events = response.data
-    } catch (e) {
-      console.log(e)
+    watchEffect(async () => {
+      this.events = null
+      try {
+        const response = await EventService.getEvents(this.perPage, this.page)
+        this.events = response.data
+        this.totalEvents = response.headers['x-total-count']
+      } catch (e) {
+        console.log(e)
+      }
+    })
+  },
+  computed: {
+    hasNextPage() {
+      return this.page < Math.ceil(this.totalEvents / 2)
     }
   }
 }
@@ -36,5 +66,14 @@ export default {
   display: flex;
   flex-direction: column;
   align-items: center;
+}
+.pagination {
+  display: flex;
+  justify-content: space-between;
+  width: 290px;
+}
+.pagination a {
+  text-decoration: none;
+  color: #2c3e50;
 }
 </style>
