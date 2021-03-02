e3677ca9d14c7c0d76237897b0fea36d11d64bc5
Carlton Joseph <carlton.joseph@gmail.com>
2021-03-02 14:59:56 -0800

    add programatic routing

--- src/views/event/Register.vue
+++ src/views/event/Register.vue
@@ -1,11 +1,19 @@
 <template>
   <div v-if="event">
     <p>Register for the event</p>
+    <button @click="register">Register Me</button>
   </div>
 </template>
 
 <script>
 export default {
-  props: ['event']
+  props: ['event'],
+  methods: {
+    register() {
+      // call api
+      //this.$router.push({ name: 'EventDetails', params: { id: this.event.id } })
+      this.$router.push({ name: 'EventDetails' })
+    }
+  }
 }
 </script>
